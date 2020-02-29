d0c2e8158152b8f8c13ebd885b16432c302bea83
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2020-02-29 10:41:18 +0100

    Show flash messages

--- lib/my_phoenix_app_web/controllers/page_controller.ex
+++ lib/my_phoenix_app_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule MyPhoenixAppWeb.PageController do
   use MyPhoenixAppWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Let's pretend we have an error.")
+    |> render("index.html")
   end
 end
