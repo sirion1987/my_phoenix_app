1ad9201094347b89a03214577057fa41d52f6f7a
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2020-02-29 11:12:33 +0100

    Set variables through the assign function

--- lib/my_phoenix_app_web/controllers/home_controller.ex
+++ lib/my_phoenix_app_web/controllers/home_controller.ex
@@ -1,11 +1,19 @@
 defmodule MyPhoenixAppWeb.HelloController do
   use MyPhoenixAppWeb, :controller
 
+  plug :assign_welcome_message, "Welcome back" when action in [:show]
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
   def show(conn, %{"id" => id}) do
-    render(conn, "show.html", messenger: id)
+    conn
+    |> assign(:name, String.capitalize(id))
+    |> render("show.html")
+  end
+
+  defp assign_welcome_message(conn, msg) do
+    assign(conn, :welcome_message, msg)
   end
 end
